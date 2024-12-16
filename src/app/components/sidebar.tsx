434843b605a3c8714a4356d9a222dfe2e75e099c
Isaac Navarro <isaacnavarro2000@gmail.com>
2024-12-16 16:45:56 -0500

    feat: creacion de sidebar e instalacion de shadcn/ui y lucide-react

---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import React from "react";
+import {
+  HouseIcon,
+  PackageIcon,
+  ReceiptText,
+  TruckIcon,
+  UsersIcon,
+} from "lucide-react";
+
+function Sidebar() {
+  return (
+    <div className="bg-gray-100 w-1/4 h-full p-4">
+      <h1 className="text-2xl font-bold mb-3">Gestor</h1>
+
+      <nav>
+        <ul className="space-y-4">
+          <li>
+            <Link
+              href="/"
+              className="flex items-center hover:bg-slate-200 rounded-md p-3 gap-2"
+            >
+              <HouseIcon />
+
+              <p className="text-lg">Inicio</p>
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/transactions"
+              className="flex items-center hover:bg-slate-200 rounded-md p-3 gap-2"
+            >
+              <ReceiptText />
+
+              <p className="text-lg">Transacciones</p>
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/products"
+              className="flex items-center hover:bg-slate-200 rounded-md p-3 gap-2"
+            >
+              <PackageIcon />
+
+              <p className="text-lg">Productos</p>
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/clients"
+              className="flex items-center hover:bg-slate-200 rounded-md p-3 gap-2"
+            >
+              <UsersIcon />
+
+              <p className="text-lg">Clientes</p>
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/suppliers"
+              className="flex items-center hover:bg-slate-200 rounded-md p-3 gap-2"
+            >
+              <TruckIcon />
+
+              <p className="text-lg">Proveedores</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Sidebar;
